a990262765dd9e95e1a2b0b32683801dd39ce174
Perry Brandiezs <pbrandiezs@gmail.com>
2021-08-04 10:04:11 -0700

    feat: Lesson 282 Transforming Response Data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
 
@@ -24,6 +25,11 @@ export class DataStorageService {
             .get<Recipe[]>(
                 'https://ng-course-recipe-book-dd9ba-default-rtdb.firebaseio.com/recipes.json'
             )
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                });
+            }))
             .subscribe(recipes => {
                 this.recipesService.setRecipes(recipes);
             })
