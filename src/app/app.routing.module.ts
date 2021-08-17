4f5d53c083f92929adb2428ff682a61798aa32b0
Perry Brandiezs <pbrandiezs@gmail.com>
2021-08-17 09:04:04 -0700

    feat: Lesson 287 Adding the Auth Page

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,6 +7,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -25,6 +26,7 @@ const appRoutes: Routes = [
         }
     ] },
     { path: 'shopping-list', component: ShoppingListComponent },
+    { path: 'auth', component: AuthComponent }
 ];
 
 @NgModule({
