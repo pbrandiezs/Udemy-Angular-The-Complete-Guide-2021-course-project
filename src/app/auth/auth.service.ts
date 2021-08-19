37a41e9a5c962affdc18686f2babfb5a0615700c
Perry Brandiezs <pbrandiezs@gmail.com>
2021-08-19 08:30:23 -0700

    feat: Lesson 296 Sending Login Requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -35,5 +36,16 @@ export class AuthService {
             }
             return throwError(errorMessage);
         }));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBuKOGkoZmypVp7cYBl74-S0_7bi2zIwyQ',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        );
     }
 }
