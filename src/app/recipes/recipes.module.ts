7a44bd87bf3ab66dae7499e32c8e0abbcc52a866
Perry Brandiezs <pbrandiezs@gmail.com>
2021-08-25 09:07:47 -0700

    feat: Lesson 322 Splitting Modules Correctly

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+
+
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RecipesComponent } from './recipes.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 
 
 @NgModule({
@@ -17,6 +22,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
         RecipeStartComponent,
         RecipeEditComponent,
     ],
+    imports: [RouterModule, CommonModule, ReactiveFormsModule],
     exports: [
         RecipesComponent,
         RecipeListComponent,
