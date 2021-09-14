adc18a3b430d8c1e25d5eec37b5526a646802aff
Perry Brandiezs <pbrandiezs@gmail.com>
2021-09-14 09:56:56 -0700

    feat: Lesson 380 Fetching Recipes and Using the Resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
 import { Recipe } from './recipe.model';
-import { DataStorageService } from '../shared/data-storage.service';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { RecipeService } from './recipe.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
+import { Actions, ofType } from '@ngrx/effects';
+import { take } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-    constructor(private dataStorageService: DataStorageService, private recipesService: RecipeService) {}
+  constructor(
+    private store: Store<fromApp.AppState>,
+    private actions$: Actions
+  ) {}
 
-    resolve(route: ActivatedRouteSnapshot, sate: RouterStateSnapshot) {
-        const recipes = this.recipesService.getRecipes();
-
-        if (recipes.length === 0) {
-            return this.dataStorageService.fetchRecipes();
-        } else {
-            return recipes;
-        }
-    }
+  resolve(route: ActivatedRouteSnapshot, sate: RouterStateSnapshot) {
+    // return this.dataStorageService.fetchRecipes();
+    this.store.dispatch(new RecipesActions.FetchRecipes());
+    return this.actions$.pipe(
+      ofType(RecipesActions.SET_RECIPES),
+      take(1)
+    );
+  }
 }
