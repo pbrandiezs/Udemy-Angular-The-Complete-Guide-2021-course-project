ab207710714b9e890e4a33b487a5e8b8eb92b075
Perry Brandiezs <pbrandiezs@gmail.com>
2021-09-14 10:48:25 -0700

    feat: Lesson 381 Fixing the Auth Redirect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -20,16 +20,22 @@ export interface AuthResponseData {
   registered?: boolean;
 }
 
-const handleAuthentication = (expiresIn: number, email: string, userId: string, token: string) => {
+const handleAuthentication = (
+  expiresIn: number,
+  email: string,
+  userId: string,
+  token: string
+) => {
   const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
   const user = new User(email, userId, token, expirationDate);
   localStorage.setItem('userData', JSON.stringify(user));
   return new AuthActions.AuthenticateSuccess({
-      email: email,
-      userId: userId,
-      token: token,
-      expirationDate: expirationDate,
-    });
+    email: email,
+    userId: userId,
+    token: token,
+    expirationDate: expirationDate,
+    redirect: true
+  });
 };
 
 const handleError = (errorRes: any) => {
@@ -123,8 +129,10 @@ export class AuthEffects {
   @Effect({ dispatch: false })
   authRedirect = this.actions$.pipe(
     ofType(AuthActions.AUTHENTICATE_SUCCESS),
-    tap(() => {
-      this.router.navigate(['/']);
+    tap((authSuccessAction: AuthActions.AuthenticateSuccess) => {
+      if (authSuccessAction.payload.redirect) {
+        this.router.navigate(['/']);
+      }
     })
   );
 
@@ -159,7 +167,8 @@ export class AuthEffects {
                 email: loadedUser.email,
                 userId: loadedUser.id,
                 token: loadedUser.token,
-                expirationDate: new Date(userData._tokenExpirationDate)
+                expirationDate: new Date(userData._tokenExpirationDate),
+                redirect: false
         });
         // const expirationDuration =
         //     new Date(userData._tokenExpirationDate).getTime() -
